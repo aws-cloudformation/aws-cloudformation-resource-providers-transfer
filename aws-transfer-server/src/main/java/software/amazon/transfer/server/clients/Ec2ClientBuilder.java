ca6a893614276dea6920b7849b43c6576ad15fac
Alex Volanis <avolanis@amazon.com>
2024-06-29 23:35:35 -0400

    Add retry strategy for throttling in clients
    This is expected for Review Readiness of the Uluru handlers.

--- aws-transfer-server/src/main/java/software/amazon/transfer/server/clients/Ec2ClientBuilder.java
+++ aws-transfer-server/src/main/java/software/amazon/transfer/server/clients/Ec2ClientBuilder.java
@@ -1,16 +1,36 @@
 package software.amazon.transfer.server.clients;
 
+import java.time.Duration;
+
+import software.amazon.awssdk.awscore.retry.AwsRetryStrategy;
+import software.amazon.awssdk.core.client.config.ClientOverrideConfiguration;
+import software.amazon.awssdk.core.internal.retry.SdkDefaultRetrySetting;
+import software.amazon.awssdk.retries.api.BackoffStrategy;
+import software.amazon.awssdk.retries.api.RetryStrategy;
 import software.amazon.awssdk.services.ec2.Ec2Client;
 import software.amazon.cloudformation.LambdaWrapper;
 
 /** Create an Ec2Client instance. */
 public class Ec2ClientBuilder {
+    private static final BackoffStrategy EC2_BACKOFF_THROTTLING_STRATEGY =
+            BackoffStrategy.exponentialDelay(Duration.ofMillis(2000), SdkDefaultRetrySetting.MAX_BACKOFF);
+
+    private static final RetryStrategy EC2_RETRY_STRATEGY = AwsRetryStrategy.adaptiveRetryStrategy().toBuilder()
+            .backoffStrategy(EC2_BACKOFF_THROTTLING_STRATEGY)
+            .maxAttempts(4)
+            .build();
+
     /**
      * Returns the Ec2Client instance.
      *
      * @return the Ec2Client instance.
      */
     public static Ec2Client getClient() {
-        return Ec2Client.builder().httpClient(LambdaWrapper.HTTP_CLIENT).build();
+        return Ec2Client.builder()
+                .httpClient(LambdaWrapper.HTTP_CLIENT)
+                .overrideConfiguration(ClientOverrideConfiguration.builder()
+                        .retryStrategy(EC2_RETRY_STRATEGY)
+                        .build())
+                .build();
     }
 }
